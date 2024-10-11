7e15a1dd42b51cab63c6eb4db82440ca0e8e5286
Pawan Kumar Singh <161737976+codecraftsmanpawan@users.noreply.github.com>
2024-10-11 13:24:34 +0530

    up

---
+++ src/components/SuperAdmin/AddMasterId.js
@@ -0,0 +1,277 @@
+import React, { useState } from "react";
+import axios from "axios";
+import {
+  FaUser,
+  FaLock,
+  FaDollarSign,
+  FaCode,
+  FaPercentage,
+  FaTags,
+  FaEye,
+  FaEyeSlash,
+} from "react-icons/fa";
+import { v4 as uuidv4 } from "uuid";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Navbar from "./SuperAdminNav";
+
+const AddMasterAdmin = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [budget, setBudget] = useState("");
+  const [status, setStatus] = useState("active");
+  const [masterCode, setMasterCode] = useState("");
+  const [mcxBrokerageType, setMcxBrokerageType] = useState("");
+  const [mcxBrokerage, setMcxBrokerage] = useState("");
+  const [shareBrokerage, setShareBrokerage] = useState("");
+  const [pattiPercentage, setPattiPercentage] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    const token = localStorage.getItem("superAdminToken");
+
+    const data = {
+      username,
+      password,
+      budget: parseFloat(budget),
+      status,
+      master_code: masterCode,
+      mcx_brokerage_type: mcxBrokerageType,
+      mcx_brokerage: mcxBrokerageType ? parseFloat(mcxBrokerage) : null,
+      share_brokerage: parseFloat(shareBrokerage),
+      pattiPercentage: parseFloat(pattiPercentage),
+    };
+
+    try {
+      const { data: response } = await axios.post(
+        "http://13.51.178.27:5000/api/var/superAdmin/add-masterAdmin",
+        data,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      toast.success("Master Admin added successfully!");
+      // Clear form fields on successful submission
+      setUsername("");
+      setPassword("");
+      setBudget("");
+      setMasterCode("");
+      setMcxBrokerageType("");
+      setMcxBrokerage("");
+      setShareBrokerage("");
+      setPattiPercentage("");
+    } catch (err) {
+      const errorMessage = err.response?.data?.message || err.message;
+      toast.error(`Error: ${errorMessage}`);
+      console.error("Error adding master admin:", errorMessage);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const generateCredentials = () => {
+    // Auto-generate username and password
+    setUsername(`Master_${uuidv4().slice(0, 8)}`);
+    setPassword(Math.random().toString(36).slice(-8));
+  };
+
+  const togglePasswordVisibility = () => {
+    setShowPassword((prev) => !prev);
+  };
+
+  return (
+    <>
+      <Navbar />
+      <div className="flex items-center justify-center min-h-screen bg-gray-100 mt-8">
+        <div className="p-6 max-w-4xl w-full bg-white shadow-lg rounded-lg">
+          <h1 className="text-2xl font-bold mb-4 text-center">
+            Add Master Admin
+          </h1>
+          <div className="bg-gray-100 p-6 rounded-lg shadow-md">
+            <form onSubmit={handleSubmit} className="grid grid-cols-6 gap-4">
+              <div className="col-span-6 sm:col-span-3">
+                <label className="flex items-center space-x-2 font-semibold mb-1">
+                  <FaUser className="text-gray-600" />
+                  <span>Username</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Username"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  className="w-full p-2 border border-gray-300 rounded"
+                  required
+                />
+                <button
+                  type="button"
+                  onClick={generateCredentials}
+                  className="mt-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+                >
+                  Auto Generate
+                </button>
+              </div>
+
+              <div className="col-span-6 sm:col-span-3 relative mb-3">
+                <label className="flex items-center space-x-2 font-semibold mb-1">
+                  <FaLock className="text-gray-600" />
+                  <span>Password</span>
+                </label>
+                <input
+                  type={showPassword ? "text" : "password"}
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="w-full p-2 border border-gray-300 rounded"
+                  required
+                />
+                <button
+                  type="button"
+                  onClick={togglePasswordVisibility}
+                  className="absolute inset-y-0 right-0 pr-3 flex items-center text-gray-600"
+                >
+                  {showPassword ? <FaEyeSlash /> : <FaEye />}
+                </button>
+              </div>
+
+              <div className="col-span-6 sm:col-span-3">
+                <label className="flex items-center space-x-2 font-semibold mb-1">
+                  <FaDollarSign className="text-gray-600" />
+                  <span>Budget</span>
+                </label>
+                <input
+                  type="number"
+                  placeholder="Budget"
+                  value={budget}
+                  onChange={(e) => setBudget(e.target.value)}
+                  className="w-full p-2 border border-gray-300 rounded"
+                  required
+                />
+              </div>
+
+              <div className="col-span-6 sm:col-span-3">
+                <label className="flex items-center space-x-2 font-semibold mb-1">
+                  <FaCode className="text-gray-600" />
+                  <span>Master Code</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Master Code"
+                  value={masterCode}
+                  onChange={(e) => setMasterCode(e.target.value)}
+                  className="w-full p-2 border border-gray-300 rounded"
+                  required
+                />
+              </div>
+
+              <div className="col-span-6 mt-3">
+                <label className="flex items-center space-x-2 font-semibold mb-4">
+                  <FaPercentage className="text-gray-600" />
+                  <span>MCX Brokerage Type</span>
+                </label>
+                <div className="flex space-x-4">
+                  <label className="flex items-center space-x-2">
+                    <input
+                      type="radio"
+                      value="per_crore"
+                      checked={mcxBrokerageType === "per_crore"}
+                      onChange={(e) => setMcxBrokerageType(e.target.value)}
+                      className="form-radio"
+                    />
+                    <span>Per Crore</span>
+                  </label>
+                  <label className="flex items-center space-x-2">
+                    <input
+                      type="radio"
+                      value="per_sauda"
+                      checked={mcxBrokerageType === "per_sauda"}
+                      onChange={(e) => setMcxBrokerageType(e.target.value)}
+                      className="form-radio"
+                    />
+                    <span>Per Sauda</span>
+                  </label>
+                </div>
+              </div>
+
+              {mcxBrokerageType && (
+                <div className="col-span-6 sm:col-span-3 mt-3">
+                  <label className="flex items-center space-x-2 font-semibold mb-1">
+                    <FaTags className="text-gray-600" />
+                    <span>MCX Brokerage</span>
+                  </label>
+                  <input
+                    type="number"
+                    placeholder="MCX Brokerage"
+                    value={mcxBrokerage}
+                    onChange={(e) => setMcxBrokerage(e.target.value)}
+                    className="w-full p-2 border border-gray-300 rounded"
+                    required
+                  />
+                </div>
+              )}
+
+              <div className="col-span-6 sm:col-span-3 mt-3">
+                <label className="flex items-center space-x-2 font-semibold mb-1">
+                  <FaTags className="text-gray-600" />
+                  <span>Share Brokerage (Per Crore)</span>
+                </label>
+                <input
+                  type="number"
+                  placeholder="Share Brokerage"
+                  value={shareBrokerage}
+                  onChange={(e) => setShareBrokerage(e.target.value)}
+                  className="w-full p-2 border border-gray-300 rounded"
+                  required
+                />
+              </div>
+
+              <div className="col-span-6 sm:col-span-3 mt-3">
+                <label className="flex items-center space-x-2 font-semibold mb-1">
+                  <FaTags className="text-gray-600" />
+                  <span>Patti (%)</span>
+                </label>
+                <input
+                  type="number"
+                  placeholder="Patti %"
+                  value={pattiPercentage}
+                  onChange={(e) => setPattiPercentage(e.target.value)}
+                  className="w-full p-2 border border-gray-300 rounded"
+                  required
+                />
+              </div>
+
+              <button
+                type="submit"
+                className="col-span-6 py-2 px-4 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+                disabled={loading}
+              >
+                {loading ? "Submitting..." : "Submit"}
+              </button>
+            </form>
+          </div>
+
+          <ToastContainer
+            position="top-right"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AddMasterAdmin;
---
+++ src/components/SuperAdmin/QuantityLimit.js
@@ -0,0 +1,160 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useParams, useNavigate } from "react-router-dom";
+import Navbar from "./SuperAdminNav";
+
+const StockForm = () => {
+  const { instrumentIdentifier, stockName } = useParams();
+  const [limit, setLimit] = useState("");
+  const [lotSize, setLotSize] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const token = localStorage.getItem("superAdminToken");
+
+    const data = JSON.stringify({
+      symbol: instrumentIdentifier,
+      limit: Number(limit),
+      lotSize: Number(lotSize),
+    });
+
+    const config = {
+      method: "post",
+      maxBodyLength: Infinity,
+      url: "http://13.51.178.27:5000/api/var/superAdmin/items",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      data: data,
+    };
+
+    try {
+      const response = await axios.request(config);
+      toast.success("Data submitted successfully!", {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+      console.log(JSON.stringify(response.data));
+
+      // Navigate to the next page after success
+      navigate("/Mange/Quantity/Limit");
+    } catch (error) {
+      if (
+        error.response &&
+        error.response.data &&
+        error.response.data.error === "Item with this symbol already exists"
+      ) {
+        toast.error("Item with this symbol already exists!", {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+      } else {
+        toast.error("Error submitting data!", {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+      }
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <Navbar />
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white shadow-lg rounded px-8 pt-6 pb-8 mb-4 w-full max-w-sm"
+      >
+        <h2 className="text-2xl font-bold text-center mb-6">
+          Set Quantity Limit
+        </h2>
+
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="symbol"
+          >
+            Symbol
+          </label>
+          <input
+            id="symbol"
+            type="text"
+            value={stockName}
+            onChange={() => {}}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            placeholder="AAPL"
+            readOnly
+          />
+        </div>
+
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="limit"
+          >
+            Limit
+          </label>
+          <input
+            id="limit"
+            type="number"
+            value={limit}
+            onChange={(e) => setLimit(e.target.value)}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            placeholder="100"
+            required
+          />
+        </div>
+
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="lotSize"
+          >
+            Lot Size
+          </label>
+          <input
+            id="lotSize"
+            type="number"
+            value={lotSize}
+            onChange={(e) => setLotSize(e.target.value)}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            placeholder="10"
+            required
+          />
+        </div>
+
+        <div className="flex items-center justify-between">
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+      <ToastContainer />
+    </div>
+  );
+};
+
+export default StockForm;
